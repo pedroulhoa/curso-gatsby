647c14ee4891d2b6f552cf5c961ff85096ddcbe4
Pedro Ulhoa <pedroulhoa@live.com>
2020-08-20 22:26:12 -0300

    feat: styled components / att Avatar, Layout, Profile

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `My Blog`,
+    title: `Pedro Ulhoa`,
     description: `A blog about frontend development and other coll stuff.`,
-    author: `@myblog`,
+    author: `@pedroulhoa`,
     position: `Software Developer`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -25,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
