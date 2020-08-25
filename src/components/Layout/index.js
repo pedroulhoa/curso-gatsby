3229498d9d95795230c4f98f1d99e1160fe41e2f
Pedro Ulhoa <pedroulhoa@live.com>
2020-08-24 21:41:37 -0300

    fix: Layout and imports / add styled icons

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
-import Profile from "../Profile"
+
+import Sidebar from "../Sidebar"
 
 import * as S from "./styled"
 
@@ -10,9 +11,7 @@ const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper>
       <GlobalStyles />
-      <aside>
-        <Profile />
-      </aside>
+      <Sidebar />
       <S.LayoutMain>{children}</S.LayoutMain>
     </S.LayoutWrapper>
   )
