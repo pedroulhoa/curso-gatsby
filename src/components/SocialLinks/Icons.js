0f35ade7df65c3f5d937fb1a87979becfa3efd8b
Pedro Ulhoa <pedroulhoa@live.com>
2020-08-24 21:42:57 -0300

    feat: SocialLinks component

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,15 @@
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Twitter } from "@styled-icons/boxicons-logos/Twitter"
+import { LinkedinSquare } from "@styled-icons/boxicons-logos/LinkedinSquare"
+import { Instagram } from "@styled-icons/boxicons-logos/Instagram"
+import { Twitch } from "@styled-icons/boxicons-logos/Twitch"
+
+const Icons = {
+  Github,
+  Twitter,
+  LinkedinSquare,
+  Instagram,
+  Twitch,
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/pedroulhoa",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/_ulhoa",
+  },
+  {
+    label: "LinkedinSquare",
+    url: "https://linkedin.com/in/pedroulhoa",
+  },
+  {
+    label: "Instagram",
+    url: "https://instagram.com/pedroulhoa",
+  },
+  {
+    label: "Twitch",
+    url: "https://twitch.tv/ulhoa",
+  },
+]
+
+export default links
