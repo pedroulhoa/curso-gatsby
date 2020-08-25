573b896d741c792ab7425de1ad5dbf37b564b91e
Pedro Ulhoa <pedroulhoa@live.com>
2020-08-25 20:12:34 -0300

    feat: PostItem Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,27 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import PostItem from "../components/PostItem"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
+    <PostItem />
   </Layout>
 )
 
