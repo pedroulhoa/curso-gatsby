fb700513ba72c08debe32acb25e76ad54e72db58
Pedro Ulhoa <pedroulhoa@live.com>
2020-09-20 00:31:43 -0300

    feat: style grid post list

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -6,6 +6,8 @@ import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
 import Pagination from "../components/Pagination"
 
+import * as S from "../components/ListWrapper/styled"
+
 const BlogList = props => {
   const postList = props.data.allMarkdownRemark.edges
 
@@ -18,29 +20,27 @@ const BlogList = props => {
   return (
     <Layout>
       <SEO title="Home" />
-      {postList.map(
-        (
-          {
+      <S.ListWrapper>
+        {postList.map(
+          ({
             node: {
               frontmatter: { background, category, date, description, title },
               timeToRead,
               fields: { slug },
             },
-          },
-          index
-        ) => (
-          <PostItem
-            key={index}
-            slug={slug}
-            background={background}
-            category={category}
-            date={date}
-            timeToRead={timeToRead}
-            title={title}
-            description={description}
-          />
-        )
-      )}
+          }) => (
+            <PostItem
+              slug={slug}
+              background={background}
+              category={category}
+              date={date}
+              timeToRead={timeToRead}
+              title={title}
+              description={description}
+            />
+          )
+        )}
+      </S.ListWrapper>
 
       <Pagination
         isFirst={isFirst}
