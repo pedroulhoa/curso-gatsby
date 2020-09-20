31cb103518785066ab6c55696493ae870f531bf7
Pedro Ulhoa <pedroulhoa@live.com>
2020-09-20 14:48:29 -0300

    fix: color bg transition by theme

--- src/components/RecommendedPosts/index.js
+++ src/components/RecommendedPosts/index.js
@@ -1,6 +1,9 @@
 import React from "react"
 import propTypes from "prop-types"
+
 import * as S from "./styled"
+
+import getThemeColor from "../../utils/getThemeColor"
 
 const RecommendedPosts = ({ next, previous }) => (
   <S.RecommendedWrapper>
@@ -9,7 +12,7 @@ const RecommendedPosts = ({ next, previous }) => (
         to={previous.fields.slug}
         cover
         direction="left"
-        bg="#16202c"
+        bg={getThemeColor()}
         className="previous"
       >
         {previous.frontmatter.title}
@@ -20,7 +23,7 @@ const RecommendedPosts = ({ next, previous }) => (
         to={next.fields.slug}
         cover
         direction="right"
-        bg="#16202c"
+        bg={getThemeColor()}
         className="next"
       >
         {next.frontmatter.title}
